4efa0c7d114b3a3582af49524793d2e5893e8837
Benjamin Lee Norman <copperbeard93@gmail.com>
2018-01-22 08:39:19 -0600

    fixed bug with round delay

--- SoftwareGuildLessons/src/main/java/com/sg/softwareguild/assignment1/Roshambo.java
+++ SoftwareGuildLessons/src/main/java/com/sg/softwareguild/assignment1/Roshambo.java
@@ -19,9 +19,10 @@ import javafx.stage.Stage;
 
 import static com.sg.softwareguild.assignment1.Weapon.*;
 import static com.sg.softwareguild.assignment1.RoundResult.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
-import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -46,16 +47,14 @@ public class Roshambo extends Application {
 
     private Stage primaryStage;
 
-    private final ImageView bImage = new ImageView("background.png");
-    /**
-     * ****************************************************************************************************
-     */
+    private final ImageView bImage1 = new ImageView("background.png");
+    private final ImageView bImage2 = new ImageView("background.png");
+    private final ImageView bImage3 = new ImageView("background.png");
+    private final ImageView bImage4 = new ImageView("background.png");
+
     /**
      * ************************************** ENTRY SCREEN
      * *************************************
-     */
-    /**
-     * ****************************************************************************************************
      */
     private final Label title = new Label("Rock! Paper! Scissors!"); // TODO: image view
     private final Button playButton = new ImageButton("playButton.png");
@@ -64,14 +63,8 @@ public class Roshambo extends Application {
     private final Scene entryScene = new Scene(entryScreen, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     /**
-     * ****************************************************************************************************
-     */
-    /**
      * *********************************** ROUND CHOICE SCREEN
      * *********************************
-     */
-    /**
-     * ****************************************************************************************************
      */
     private final Button[] roundChoices = new Button[10];
     private final IntegerProperty numRounds = new SimpleIntegerProperty();
@@ -80,26 +73,17 @@ public class Roshambo extends Application {
     private final Scene roundChoiceScene = new Scene(roundChoiceScreen, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     /**
-     * ****************************************************************************************************
-     */
-    /**
      * *************************************** GAME SCREEN
      * *************************************
      */
-    /**
-     * ****************************************************************************************************
-     */
     private Weapon currentUserChoice;
     private Weapon currentComputerChoice;
-    private RoundResult currentRoundWinner;
 
     private final Image rockIcon = new Image("rock.png");
     private final Image paperIcon = new Image("paper.png");
     private final Image scissorsIcon = new Image("scissors.png");
     private final Image loading = new Image("loading3.gif");
 
-    private final Label winnerLabel = new Label("Winner");
-    private final Label looserLabel = new Label("Loser");
     private final Label roundNumber = new Label(); // round + i
 
     private final Button rockButton = new ImageButton("rock.png"); // todo image button but pass in image view
@@ -115,23 +99,14 @@ public class Roshambo extends Application {
     private final Scene gameScene = new Scene(gameScreen, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     /**
-     * ****************************************************************************************************
-     */
-    /**
      * ************************************* RESULTS SCREEN
      * ************************************
-     */
-    /**
-     * ****************************************************************************************************
      */
     private final Button playAgianButton = new Button("Play Agian?");
     private final Button exitToTitleScreen = new Button("Exit");
     private final IntegerProperty computerWins = new SimpleIntegerProperty();
     private final IntegerProperty userWins = new SimpleIntegerProperty();
     private final IntegerProperty ties = new SimpleIntegerProperty();
-    private final Label userWinsLabel = new Label("User Wins: ");
-    private final Label computerWinsLabel = new Label("Computer Wins: ");
-    private final Label tiesLabel = new Label("Ties: ");
     private final Pane resultsScreen = new Pane();
     private final Scene resultsScene = new Scene(resultsScreen, SCREEN_WIDTH, SCREEN_HEIGHT);
 
@@ -139,56 +114,27 @@ public class Roshambo extends Application {
      * moved event handlers up so I can sort of read down the event list that
      * will be called in order
      */
-    EventHandler<MouseEvent> exitButtonEvent = (MouseEvent event) -> {
-        // TODO: display thanks for playing
+    private final EventHandler<MouseEvent> exitButtonEvent = (MouseEvent event) -> {
+        // no need to display thanks for playing
         System.exit(0);
     };
 
-    EventHandler<MouseEvent> playButtonHandler = (MouseEvent event) -> {
+    private final EventHandler<MouseEvent> playButtonHandler = (MouseEvent event) -> {
         primaryStage.setScene(roundChoiceScene);
     };
 
-    EventHandler<MouseEvent> roundNumButtonHandler = (MouseEvent event) -> {
+    private final EventHandler<MouseEvent> roundNumButtonHandler = (MouseEvent event) -> {
         // set the number of rounds the user chose to whatever button was clicked (1 to 10)
         Button source = (Button) event.getSource();
         numRounds.set(Integer.parseInt(source.getText()));
+        // user cant choose a invalid value so no need to handle errors
 
         primaryStage.setScene(gameScene);
-        // TODO: anim out offscreen
-        // TODO: anim in offscreen
-        // TODO: show weapon anim onscren
     };
 
-    EventHandler<ActionEvent> testHandler = (ActionEvent event) -> {
-        currentRoundWinner = getRoundResult();
-
-        switch (currentRoundWinner) {
-            case COMPUTER_WIN:
-                computerWins.set(computerWins.get() + 1);
-                break;
-            case TIE:
-                ties.set(ties.get() + 1);
-                break;
-            case USER_WIN:
-                userWins.set(userWins.get() + 1);
-                break;
-        }
-
-        if (numRounds.get() > 1) {
-            // TODO: next round anim
-
-            numRounds.set(numRounds.get() - 1);
-        } else {
-            // TODO: show results
-            // TODO: continue button // move below to continue button listener
-            // TODO: show play agian buttons
-            // TODO: ask to play agian
-        }
-    };
-
-    EventHandler<MouseEvent> weaponButtonHandler = (MouseEvent event) -> {
+    private final EventHandler<MouseEvent> weaponButtonHandler = (MouseEvent event) -> {
         Button source = (Button) event.getSource();
-        // buttons text is the same as the enums string value
+
         if (source == rockButton) {
             currentUserChoice = ROCK;
         } else if (source == paperButton) {
@@ -197,20 +143,19 @@ public class Roshambo extends Application {
             currentUserChoice = SCISSORS;
         }
 
-        // TODO: slide out non choice then choice
-        // TODO: Start countdown anim (keyframe 1)
         currentComputerChoice = calculateComputerChoice();
         switch (getRoundResult()) {
             case COMPUTER_WIN:
                 computerWins.set(computerWins.get() + 1);
+                break;
             case TIE:
                 ties.set(ties.get() + 1);
+                break;
             case USER_WIN:
                 userWins.set(userWins.get() + 1);
-            default: // should never happen
+                break;
         }
 
-        // TODO: show winner anim
         switch (currentUserChoice) {
             case ROCK:
                 userChoice.setImage(rockIcon);
@@ -222,10 +167,12 @@ public class Roshambo extends Application {
                 userChoice.setImage(scissorsIcon);
                 break;
         }
+
+        // hide the buttons after use makes there choice
         rockButton.setVisible(false);
         paperButton.setVisible(false);
         scissorsButton.setVisible(false);
-
+        
         Task<Void> sleeper = new Task<Void>() {
             @Override
             protected Void call() throws Exception {
@@ -237,6 +184,7 @@ public class Roshambo extends Application {
                 return null;
             }
         };
+        
         Task<Void> sleeper2 = new Task<Void>() {
             @Override
             protected Void call() throws Exception {
@@ -248,6 +196,8 @@ public class Roshambo extends Application {
                 return null;
             }
         };
+        
+        // after the first delay show the computer choice
         sleeper.setOnSucceeded((WorkerStateEvent event1) -> {
             switch (currentComputerChoice) {
                 case ROCK:
@@ -260,9 +210,14 @@ public class Roshambo extends Application {
                     computerChoice.setImage(scissorsIcon);
                     break;
             }
+            // TODO: darken loser brighten winner
+            // need to add effect to player/computer icon
+            // start the second delay
             new Thread(sleeper2).start();
         });
-        sleeper2.setOnSucceeded((WorkerStateEvent event1) -> {
+
+        // after the second delay move to next round
+        sleeper2.setOnSucceeded((WorkerStateEvent event2) -> {
             rockButton.setVisible(true);
             paperButton.setVisible(true);
             scissorsButton.setVisible(true);
@@ -274,6 +229,7 @@ public class Roshambo extends Application {
                 numRounds.set(numRounds.get() - 1);
             }
         });
+        // start the first delay
         new Thread(sleeper).start();
     };
 
@@ -293,13 +249,14 @@ public class Roshambo extends Application {
     private void initStartScreen() {
         entryScreen.setAlignment(Pos.CENTER);
 
-        // for some reason bImage is being ignored.... dont know why
         HBox buttons = new HBox(playButton, exitButton);
         buttons.setAlignment(Pos.CENTER);
         buttons.setPrefHeight(200);
+
         VBox contentsEntry = new VBox(title, buttons);
         contentsEntry.setAlignment(Pos.CENTER);
-        entryScreen.getChildren().addAll(bImage, contentsEntry);
+
+        entryScreen.getChildren().addAll(bImage1, contentsEntry);
 
         playButton.setOnMouseClicked(playButtonHandler);
 
@@ -313,17 +270,17 @@ public class Roshambo extends Application {
         rounds.setAlignment(Pos.CENTER);
         VBox contents = new VBox(chooseRoundNum, rounds);
         contents.setAlignment(Pos.CENTER);
-        roundChoiceScreen.getChildren().addAll(bImage, contents);
+        roundChoiceScreen.getChildren().addAll(bImage2, contents);
 
         for (int i = 0; i < roundChoices.length; i++) {
-            // todo custom from url+"i"
+            // TODO: images
             Button button = new Button((i + 1) + "");
             rounds.getChildren().add(button);
             button.setOnMouseClicked(roundNumButtonHandler);
             roundChoices[i] = button;
         }
 
-        class MyNSC extends NumberStringConverter {
+        class RoundNumberStringConverter extends NumberStringConverter {
 
             @Override
             public String toString(Number number) {
@@ -331,12 +288,14 @@ public class Roshambo extends Application {
                 return (round != 0) ? "Rounds Left: " + round : "Final Round";
             }
 
+            @Override
             public Number fromString(String string) {
                 return numRounds.get();
             }
         }
 
-        this.roundNumber.textProperty().bindBidirectional(numRounds, new MyNSC());
+        this.roundNumber.textProperty().bindBidirectional(numRounds, new RoundNumberStringConverter());
+
     }
 
     private void initGameScreen() {
@@ -345,33 +304,86 @@ public class Roshambo extends Application {
         scissorsButton.setOnMouseClicked(weaponButtonHandler);
 
         GridPane grid = new GridPane();
-        // grid.setGridLinesVisible(true);
-
         grid.setAlignment(Pos.CENTER);
 
+        // add user side
         grid.addColumn(1, userIcon, userChoice);
         grid.addRow(2, rockButton, paperButton, scissorsButton);
 
+        // add computer side
         grid.addColumn(5, computerIcon, computerChoice);
-
         grid.add(roundNumber, 3, 0, 3, 1);
 
+        // constrain columns and rows
         for (int i = 0; i < 7; i++) {
             grid.getColumnConstraints().add(new ColumnConstraints(SCREEN_WIDTH / 7));
-            //  grid.getColumnConstraints().get(i).setMinWidth(SCREEN_WIDTH/7);
         }
         for (int i = 0; i < 3; i++) {
             grid.getRowConstraints().add(new RowConstraints(SCREEN_HEIGHT / 3));
         }
 
         gameScreen.setAlignment(Pos.CENTER);
-        gameScreen.getChildren().addAll(bImage, grid);
+        gameScreen.getChildren().addAll(bImage3, grid);
     }
 
     private void initResultsScreen() {
-        VBox contents = new VBox(userWinsLabel, computerWinsLabel, tiesLabel, new HBox(playAgianButton, exitToTitleScreen));
         GridPane grid = new GridPane();
-        resultsScreen.getChildren().add(contents);
+        grid.setHgap(30);
+        grid.setVgap(30);
+        Label userWinsLabel = new Label("User Wins: ");
+        Label computerWinsLabel = new Label("Computer Wins: ");
+        Label tiesLabel = new Label("Ties: ");
+        grid.addColumn(0, userWinsLabel, computerWinsLabel, tiesLabel, playAgianButton);
+        Label label = new Label();
+        Label label1 = new Label();
+        Label label2 = new Label();
+        label.textProperty().bind(userWins.asString());
+        label1.textProperty().bind(computerWins.asString());
+        label2.textProperty().bind(ties.asString());
+        grid.addColumn(1, label, label1, label2, exitToTitleScreen);
+
+        // flip the overall winner displayed at the end to computer, user, or empty
+        ImageView winnerImage = new ImageView(userIcon.getImage());
+        userWins.addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                if (userWins.get() > computerWins.get()) {
+                    winnerImage.setVisible(true);
+                    winnerImage.setImage(userIcon.getImage());
+                } else if (userWins.get() == computerWins.get()) {
+                    winnerImage.setVisible(false);
+                }
+            }
+        });
+        computerWins.addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                if (userWins.get() < computerWins.get()) {
+                    winnerImage.setVisible(true);
+                    winnerImage.setImage(computerIcon.getImage());
+                } else if (userWins.get() == computerWins.get()) {
+                    winnerImage.setVisible(false);
+                }
+            }
+        });
+        // initially hidden, this is to deeal with case where a game concludes with only ties
+        winnerImage.setVisible(false);
+        grid.addColumn(2, new Label("---Winner---"), winnerImage);
+
+        resultsScreen.getChildren().addAll(bImage4, grid);
+
+        // clear previous games results and goto round select screen
+        playAgianButton.setOnMouseClicked(event -> {
+            userWins.set(0);
+            computerWins.set(0);
+            ties.set(0);
+            primaryStage.setScene(roundChoiceScene);
+        });
+
+        // go to title screen
+        exitToTitleScreen.setOnMouseClicked(event -> {
+            primaryStage.setScene(entryScene);
+        });
     }
 
     // TODO: pass in AI so we can change from say a random AI to one that chooses only rock
@@ -437,7 +449,7 @@ class ImageButton extends Button {
 
         setStyle("-fx-background-color: transparent;");
 
-        // on click darken the image like how a button darkens slightly on click
+        // on click: darken the image like how a button darkens slightly on click
         setOnMousePressed((MouseEvent event) -> {
             adjust.setBrightness(-0.5f);
         });
